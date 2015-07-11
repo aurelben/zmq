a99371bb71a0afacbbdfd15bd990399a5e19a453
Aurelien Pigot <aurel.pigot@gmail.com>
2015-07-11 02:16:00 +0200

    poc pub/sub zmq client and server

---
+++ serveur_pub.py
@@ -0,0 +1,21 @@
+import zmq
+import time
+import random
+import sys
+
+port = "4242"
+
+context = zmq.Context()
+
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:%s" % port)
+
+while True:
+	topic = random.randrange(9999, 10005)
+	messagedata = random.randrange(1, 215) - 80
+	print "%d %d" % (topic, messagedata)
+	socket.send("%d %d" % (topic, messagedata))
+	time.sleep(1)
+
+
+
