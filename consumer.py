4be00fd1a80299ad1f1a47a16cbb8d070b06a65a
Aurelien Pigot <aurel.pigot@gmail.com>
2015-07-11 02:54:29 +0200

    poc zmq PUSH/PULL

---
+++ consumer.py
@@ -0,0 +1,26 @@
+import time
+import zmq
+import random
+
+def consumer():
+	consumer_id = random.randrange(1,10005)
+	print "i am consumer #%s" % (consumer_id)
+	context = zmq.Context()
+
+	#revieve work from consumer
+	consumer_receiver = context.socket(zmq.PULL)
+	consumer_receiver.connect("tcp://127.0.0.1:5557")
+
+	#send work to result colector
+	consumer_sender = context.socket(zmq.PUSH)
+	consumer_sender.connect("tcp://127.0.0.1:5558")
+
+	while True:
+		work = consumer_receiver.recv_json()
+		data = work['num']
+		result = {'consumer' : consumer_id, 'num' : data}
+		if data%2 == 0:
+			consumer_sender.send_json(result)
+
+
+consumer()
